30625a924a71e9a2ebe940139489f8455e0af7cd
Bruno Penin <you@example.com>
2024-09-25 10:55:25 -0300

    Validação-compra

---
+++ cypress/e2e/carrinho.cy.js
@@ -0,0 +1,37 @@
+import Login from '../pages/login'
+import Inventory from '../pages/inventory'
+import Header from '../pages/header'
+import Cart from '../pages/cart'
+describe('Carrinho', () => {
+
+    beforeEach(() => {
+        //Arrange
+        Login.visitarPagina()
+        Login.preencherCredenciaisValidas()
+
+    })
+    it('Adicionar Produto ao carrinho com sucesso', () => {      
+        //Act
+        Inventory.adicionarProduto('Sauce Labs Backpack')
+
+        //Assert
+        const qtdItensAdicionados = 1
+        Header.validarQueCarrinhoPossuiItens(qtdItensAdicionados)
+        Header.navegarParaCarrinho()
+       
+
+        Cart.ValidarProdutoPresenteNoCarrinho('Sauce Labs Backpack')        
+    });
+
+    it('Remover produto do carrinho com sucesso', () => {
+        //Arrange
+        Inventory.adicionarProduto('Sauce Labs Backpack')
+        
+        //Act
+        Inventory.removerProduto('Sauce Labs Backpack')
+
+        //Assert
+        Header.validarQueCarrinhoNaoPossuiItens()    
+    })
+});
+
---
+++ cypress/pages/cart/index.js
@@ -0,0 +1,10 @@
+class Cart {
+
+    ValidarProdutoPresenteNoCarrinho(name) {
+        cy.contains(name).should('be.visible')
+        cy.screenshot('produto adicionado')
+    }
+    
+}
+
+export default new Cart()
